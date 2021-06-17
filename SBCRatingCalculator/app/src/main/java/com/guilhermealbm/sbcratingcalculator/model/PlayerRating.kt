caf1f88296b61490644c69b63210dae040485809
guilhermealbm <gui.miranda.13@gmail.com>
2021-06-17 17:10:36 -0300

    add PlayerRating model

---
+++ SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/model/PlayerRating.kt
@@ -0,0 +1,32 @@
+package com.guilhermealbm.sbcratingcalculator.model
+
+data class PlayerRating(
+    val rating: Int,
+    val players: Int
+)
+
+class PlayerRatingBuilder {
+    var rating: Int = 0
+    var players: Int = 0
+
+    fun build() : PlayerRating = PlayerRating(rating, players)
+}
+
+fun playerRating(block: PlayerRatingBuilder.() -> Unit): PlayerRating = PlayerRatingBuilder().apply(block).build()
+
+fun createRatings() = Array(10) { i ->
+    arrayOf(
+        playerRating {
+            rating = 70 + i
+            players = 0
+        },
+        playerRating {
+            rating = 80 + i
+            players = 0
+        },
+        playerRating {
+            rating = 90 + i
+            players = 0
+        }
+    )
+}

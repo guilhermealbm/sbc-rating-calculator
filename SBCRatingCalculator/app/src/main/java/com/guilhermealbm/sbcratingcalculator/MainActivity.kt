82af55ab7bfa23f0e19b487318b048a9f90a927d
guilhermealbm <gui.miranda.13@gmail.com>
2021-06-18 16:18:39 -0300

    add list view to main activity

--- SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/MainActivity.kt
+++ SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/MainActivity.kt
@@ -2,10 +2,17 @@ package com.guilhermealbm.sbcratingcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.guilhermealbm.sbcratingcalculator.adapters.PlayerRatingAdapter
+import com.guilhermealbm.sbcratingcalculator.model.createRatings
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        player_rating_selector_list_view.adapter = PlayerRatingAdapter(createRatings())
+        player_rating_selector_list_view.layoutManager = LinearLayoutManager(this)
     }
 }

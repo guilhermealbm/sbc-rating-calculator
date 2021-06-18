5a2e50a2681f717804dffba2e9cbc98007917317
guilhermealbm <gui.miranda.13@gmail.com>
2021-06-18 20:16:28 -0300

    Add onClickListener at add and sub player button

--- SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/adapters/PlayerRatingAdapter.kt
+++ SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/adapters/PlayerRatingAdapter.kt
@@ -3,6 +3,7 @@ package com.guilhermealbm.sbcratingcalculator.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.guilhermealbm.sbcratingcalculator.R
@@ -13,14 +14,38 @@ class PlayerRatingAdapter (private val ratings : Array<Array<PlayerRating>>) :
 
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
         private val playerSelector1: View = view.findViewById(R.id._7x_selector)
         val playerRating1: TextView = playerSelector1.findViewById(R.id.player_rating)
+        val addPlayerBtn1: Button = playerSelector1.findViewById(R.id.add_btn)
+        val subPlayerBtn1: Button = playerSelector1.findViewById(R.id.sub_btn)
 
         private val playerSelector2: View = view.findViewById(R.id._8x_selector)
         val playerRating2: TextView = playerSelector2.findViewById(R.id.player_rating)
+        val addPlayerBtn2: Button = playerSelector2.findViewById(R.id.add_btn)
+        val subPlayerBtn2: Button = playerSelector2.findViewById(R.id.sub_btn)
 
         private val playerSelector3: View = view.findViewById(R.id._9x_selector)
         val playerRating3: TextView = playerSelector3.findViewById(R.id.player_rating)
+        val addPlayerBtn3: Button = playerSelector3.findViewById(R.id.add_btn)
+        val subPlayerBtn3: Button = playerSelector3.findViewById(R.id.sub_btn)
+
+        init {
+            addSubPlayersOnClickListener(addPlayerBtn1, subPlayerBtn1, 0)
+            addSubPlayersOnClickListener(addPlayerBtn2, subPlayerBtn2, 1)
+            addSubPlayersOnClickListener(addPlayerBtn3, subPlayerBtn3, 2)
+        }
+
+
+        private fun addSubPlayersOnClickListener(addBtn: Button, subBtn: Button, index: Int) {
+            addBtn.setOnClickListener {
+                println("+ $bindingAdapterPosition $index")
+            }
+            subBtn.setOnClickListener {
+                println("- $bindingAdapterPosition $index")
+            }
+        }
+
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {

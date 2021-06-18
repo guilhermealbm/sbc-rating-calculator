f1cf5e8d93c59ee7c2955f48e55aa46841525676
guilhermealbm <gui.miranda.13@gmail.com>
2021-06-18 16:40:47 -0300

    add sbc rating calculator view model

--- SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/MainActivity.kt
+++ SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/MainActivity.kt
@@ -2,15 +2,19 @@ package com.guilhermealbm.sbcratingcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.guilhermealbm.sbcratingcalculator.adapters.PlayerRatingAdapter
 import com.guilhermealbm.sbcratingcalculator.model.createRatings
+import com.guilhermealbm.sbcratingcalculator.viewmodels.SBCRatingCalculatorViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val model: SBCRatingCalculatorViewModel by viewModels()
 
         player_rating_selector_list_view.adapter = PlayerRatingAdapter(createRatings())
         player_rating_selector_list_view.layoutManager = LinearLayoutManager(this)
---
+++ SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/viewmodels/SBCRatingCalculatorViewModel.kt
@@ -0,0 +1,7 @@
+package com.guilhermealbm.sbcratingcalculator.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class SBCRatingCalculatorViewModel : ViewModel() {
+    val playersByRating: HashMap<Int, Int> = TODO()
+}

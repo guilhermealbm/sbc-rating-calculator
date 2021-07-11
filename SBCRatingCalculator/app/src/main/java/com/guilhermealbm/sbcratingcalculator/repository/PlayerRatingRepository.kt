f706fe4811f9011347f80a2c7a60aa0646937404
guilhermealbm <gui.miranda.13@gmail.com>
2021-07-11 12:40:40 -0300

    Add savePlayerRatingDb method

--- SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/repository/PlayerRatingRepository.kt
+++ SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/repository/PlayerRatingRepository.kt
@@ -1,6 +1,7 @@
 package com.guilhermealbm.sbcratingcalculator.repository
 
 import com.guilhermealbm.sbcratingcalculator.data.PlayerRatingDao
+import com.guilhermealbm.sbcratingcalculator.model.PlayerRating
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -8,5 +9,7 @@ import javax.inject.Singleton
 class PlayerRatingRepository @Inject constructor(private val playerRatingDao: PlayerRatingDao) {
 
     fun getPlayersRating() = playerRatingDao.load()
+
+    suspend fun savePlayerRating(rating: Int, players: Int) = playerRatingDao.save(playerRating = PlayerRating(rating, players))
 
 }
--- SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/viewmodels/SBCRatingCalculatorViewModel.kt
+++ SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/viewmodels/SBCRatingCalculatorViewModel.kt
@@ -1,8 +1,22 @@
 package com.guilhermealbm.sbcratingcalculator.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.guilhermealbm.sbcratingcalculator.model.PlayerRating
+import com.guilhermealbm.sbcratingcalculator.repository.PlayerRatingRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SBCRatingCalculatorViewModel : ViewModel() {
-    val playersByRating: LiveData<HashMap<Int, Int>> = TODO()
+@HiltViewModel
+class SBCRatingCalculatorViewModel @Inject constructor (
+    private val playerRatingRepository: PlayerRatingRepository
+) : ViewModel() {
+
+    val playersByRating = playerRatingRepository.getPlayersRating().asLiveData()
+
+    fun savePlayerRatingDb(playerRating: PlayerRating) {
+        viewModelScope.launch {
+            playerRatingRepository.savePlayerRating(playerRating.rating, playerRating.players)
+        }
+    }
 }

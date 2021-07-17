e3cd756c310a4b573722d15cf269f69f907a774a
guilhermealbm <gui.miranda.13@gmail.com>
2021-07-17 18:24:50 -0300

    refactor adapter code to follow mvvm patterns

--- SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/adapters/PlayerRatingAdapter.kt
+++ SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/adapters/PlayerRatingAdapter.kt
@@ -1,67 +1,61 @@
 package com.guilhermealbm.sbcratingcalculator.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.guilhermealbm.sbcratingcalculator.R
+import com.guilhermealbm.sbcratingcalculator.databinding.PlayerRatingSelectorBinding
 import com.guilhermealbm.sbcratingcalculator.model.PlayerRating
 
-class PlayerRatingAdapter (private val ratings : Array<Array<PlayerRating>>) :
-    RecyclerView.Adapter<PlayerRatingAdapter.ViewHolder>() {
+class PlayerRatingAdapter : ListAdapter<PlayerRating, RecyclerView.ViewHolder>(PlayerRatingDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return PlayerRatingViewHolder(
+            PlayerRatingSelectorBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val playerRating = getItem(position)
+        (holder as PlayerRatingViewHolder).bind(playerRating)
+    }
 
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        private val playerSelector1: View = view.findViewById(R.id._7x_selector)
-        val playerRating1: TextView = playerSelector1.findViewById(R.id.player_rating)
-        val addPlayerBtn1: Button = playerSelector1.findViewById(R.id.add_btn)
-        val subPlayerBtn1: Button = playerSelector1.findViewById(R.id.sub_btn)
-
-        private val playerSelector2: View = view.findViewById(R.id._8x_selector)
-        val playerRating2: TextView = playerSelector2.findViewById(R.id.player_rating)
-        val addPlayerBtn2: Button = playerSelector2.findViewById(R.id.add_btn)
-        val subPlayerBtn2: Button = playerSelector2.findViewById(R.id.sub_btn)
-
-        private val playerSelector3: View = view.findViewById(R.id._9x_selector)
-        val playerRating3: TextView = playerSelector3.findViewById(R.id.player_rating)
-        val addPlayerBtn3: Button = playerSelector3.findViewById(R.id.add_btn)
-        val subPlayerBtn3: Button = playerSelector3.findViewById(R.id.sub_btn)
+    class PlayerRatingViewHolder(
+        private val binding: PlayerRatingSelectorBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
 
         init {
-            addSubPlayersOnClickListener(addPlayerBtn1, subPlayerBtn1, 0)
-            addSubPlayersOnClickListener(addPlayerBtn2, subPlayerBtn2, 1)
-            addSubPlayersOnClickListener(addPlayerBtn3, subPlayerBtn3, 2)
-        }
-
-
-        private fun addSubPlayersOnClickListener(addBtn: Button, subBtn: Button, index: Int) {
-            addBtn.setOnClickListener {
-                println("+ $bindingAdapterPosition $index")
-            }
-            subBtn.setOnClickListener {
-                println("- $bindingAdapterPosition $index")
+            binding.setClickListener {
+                binding.playerRating?.let { playerRating ->
+                    println("testando..." + playerRating.players + "  " + playerRating.rating)
+                }
             }
         }
 
+        fun bind(item: PlayerRating) {
+            binding.apply {
+                playerRating = item
+                executePendingBindings()
+            }
+        }
+
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.player_rating_selector_row, viewGroup, false)
+}
 
-        return ViewHolder(view)
+private class PlayerRatingDiffCallback : DiffUtil.ItemCallback<PlayerRating>() {
+
+    override fun areItemsTheSame(oldItem: PlayerRating, newItem: PlayerRating): Boolean {
+        return oldItem.rating == newItem.rating
     }
 
-
-    override fun getItemCount() = ratings.size
-
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.playerRating1.text = "${ratings[position][0].players} x ${ratings[position][0].rating}"
-        viewHolder.playerRating2.text = "${ratings[position][1].players} x ${ratings[position][1].rating}"
-        viewHolder.playerRating3.text = "${ratings[position][2].players} x ${ratings[position][2].rating}"
+    override fun areContentsTheSame(oldItem: PlayerRating, newItem: PlayerRating): Boolean {
+        return oldItem == newItem
     }
-
 }
--- SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/fragments/SBCRatingCalculatorFragment.kt
+++ SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/fragments/SBCRatingCalculatorFragment.kt
@@ -6,13 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.guilhermealbm.sbcratingcalculator.viewmodels.SBCRatingCalculatorViewModel
-import com.guilhermealbm.sbcratingcalculator.R
 import com.guilhermealbm.sbcratingcalculator.adapters.PlayerRatingAdapter
-import com.guilhermealbm.sbcratingcalculator.model.createRatings
+import com.guilhermealbm.sbcratingcalculator.databinding.SbcRatingCalculatorFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.sbc_rating_calculator_fragment.*
 
 @AndroidEntryPoint
 class SBCRatingCalculatorFragment : Fragment() {
@@ -20,39 +17,24 @@ class SBCRatingCalculatorFragment : Fragment() {
     private val viewModel: SBCRatingCalculatorViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.sbc_rating_calculator_fragment, container, false)
+
+        val binding = SbcRatingCalculatorFragmentBinding.inflate(inflater, container, false)
+        context ?: return binding.root
+
+        val adapter = PlayerRatingAdapter()
+        binding.playerRatingSelectorListView.adapter = adapter
+        subscribeUi(adapter)
+
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        player_rating_selector_list_view.layoutManager = LinearLayoutManager(context)
-        viewModel.playersByRating.observe(viewLifecycleOwner) {
-            if (it.size != 30) {
-                val newList = createRatings()
-                newList.forEach { threePositionList ->
-                    run {
-                        threePositionList.forEach { playerRating ->
-                            run {
-                                viewModel.savePlayerRatingDb(playerRating)
-                            }
-                        }
-                    }
-                }
-
-            } else {
-                val playersArray = Array(10) { i ->
-                    arrayOf(
-                        it[i],
-                        it[i + 10],
-                        it[i + 20]
-                    )
-                }
-                player_rating_selector_list_view.adapter = PlayerRatingAdapter(playersArray)
-            }
+    private fun subscribeUi(adapter: PlayerRatingAdapter) {
+        viewModel.playersByRating.observe(viewLifecycleOwner) { players ->
+            adapter.submitList(players)
         }
     }
-
 }

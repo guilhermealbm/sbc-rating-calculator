5f1dc0b8d8ff56455436f50c0728b6d06be8475d
guilhermealbm <gui.miranda.13@gmail.com>
2021-07-21 11:21:40 -0300

    add operations unit tests

---
+++ SBCRatingCalculator/app/src/test/java/com/guilhermealbm/sbcratingcalculator/utils/SBCRatingOperationsTest.kt
@@ -0,0 +1,58 @@
+package com.guilhermealbm.sbcratingcalculator.utils
+
+import com.guilhermealbm.sbcratingcalculator.model.PlayerRating
+import com.guilhermealbm.sbcratingcalculator.model.playerRating
+import com.guilhermealbm.sbcratingcalculator.util.calculateSquadRating
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class SBCRatingOperationsTest {
+
+    private lateinit var playersList: ArrayList<PlayerRating>
+
+    @Before
+    fun setUp() {
+        playersList = Array(20) { i ->
+            playerRating {
+                rating = 80 + i
+                players = 0
+            }
+        }.toList() as ArrayList<PlayerRating>
+    }
+
+    @Test
+    fun testCalculateSquadRating83() {
+        val players83 = playerRating {
+            rating = 83
+            players = 9
+        }
+        val players82 = playerRating {
+            rating = 82
+            players = 2
+        }
+
+        playersList[3] = players83
+        playersList[2] = players82
+
+        assertEquals(83, calculateSquadRating(playersList))
+    }
+
+    @Test
+    fun testCalculateSquadRating82() {
+        val players83 = playerRating {
+            rating = 83
+            players = 8
+        }
+        val players82 = playerRating {
+            rating = 82
+            players = 3
+        }
+
+        playersList[3] = players83
+        playersList[2] = players82
+
+        assertEquals(82, calculateSquadRating(playersList))
+    }
+
+}

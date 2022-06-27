92f348371e90c3601c8b54c92cd6342727d780f0
guilhermealbm <gui.miranda.13@gmail.com>
2022-06-27 19:46:48 -0300

    update viewmodel

--- SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/MainActivity.kt
+++ SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/MainActivity.kt
@@ -3,14 +3,26 @@ package com.guilhermealbm.sbcratingcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import com.guilhermealbm.sbcratingcalculator.viewmodels.SBCRatingCalculatorViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: SBCRatingCalculatorViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SBCRatingCalculatorApp()
         }
+    }
+
+    override fun onStop() {
+        viewModel.playersByRating.value?.let {
+            viewModel.savePlayersRatingDb(it)
+        }
+        super.onStop()
     }
 }
--- SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/viewmodels/SBCRatingCalculatorViewModel.kt
+++ SBCRatingCalculator/app/src/main/java/com/guilhermealbm/sbcratingcalculator/viewmodels/SBCRatingCalculatorViewModel.kt
@@ -8,6 +8,7 @@ import com.guilhermealbm.sbcratingcalculator.util.MISSING_PLAYERS
 import com.guilhermealbm.sbcratingcalculator.util.TOO_MANY_PLAYERS
 import com.guilhermealbm.sbcratingcalculator.util.calculateSquadRating
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,7 +17,14 @@ class SBCRatingCalculatorViewModel @Inject constructor (
     private val playerRatingRepository: PlayerRatingRepository
 ) : ViewModel() {
 
-    val playersByRating = playerRatingRepository.getPlayersRating().asLiveData()
+    private val _playersByRating = MutableLiveData<List<PlayerRating>>()
+    val playersByRating: LiveData<List<PlayerRating>?> = _playersByRating
+
+    init {
+        viewModelScope.launch {
+            _playersByRating.value = playerRatingRepository.getPlayersRating().first()
+        }
+    }
 
     fun savePlayersRatingDb(playerRatings: List<PlayerRating>) {
         viewModelScope.launch {
@@ -24,8 +32,21 @@ class SBCRatingCalculatorViewModel @Inject constructor (
         }
     }
 
+    fun updatePlayerInList(playerRating: PlayerRating, add: Boolean = true) {
+        val index = _playersByRating.value?.indexOf(playerRating) ?: 0
+
+        val newPlayerRating: PlayerRating = if (add)
+            PlayerRating(playerRating.rating, playerRating.players + 1)
+        else
+            PlayerRating(playerRating.rating, playerRating.players - 1)
+
+        val newList = _playersByRating.value?.toMutableList()
+        newList?.set(index, newPlayerRating)
+        _playersByRating.value = newList
+    }
+
     fun getSquadRating(players: List<PlayerRating>) : Int {
-        val playersNum = players.map { p -> p.players }.sum()
+        val playersNum = players.sumOf { p -> p.players }
         return when {
             playersNum < 11 -> MISSING_PLAYERS
             playersNum == 11 -> calculateSquadRating(players)
